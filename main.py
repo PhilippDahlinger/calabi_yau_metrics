7435297c16b4e376e85a4dc5a0c465d55d1b2822
Philipp Dahlinger <philipp.dahlinger@gmx.net>
2024-07-09 16:03:27 +0200

    very basic config setup done

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import threading
 import traceback
 
 import hydra
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
 
 from calabi_yau_metrics.util.util import load_omega_conf_resolvers
 
@@ -16,19 +16,20 @@ load_omega_conf_resolvers()
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="training_config")
-def train(config: ConfigDict) -> None:
+def train(config: DictConfig) -> None:
     try:
-        env, algorithm, evaluator, recorder = main_initialization(config)
-        for epoch in range(config.epochs):
-            training_metrics = algorithm.train_step(epoch=epoch)
-            evaluation_metrics = evaluator.eval_step(epoch=epoch)
-            # combine training and evaluation metrics
-            metrics = deep_update(training_metrics, evaluation_metrics)
-            # start a new thread to record the iteration, this speeds up the overall training
-            recorder.record_iteration(iteration=epoch, recorded_values=metrics)
-
-        # # close wandb, save the final model, ...
-        recorder.finalize()
+        print(OmegaConf.to_yaml(config))
+        # env, algorithm, evaluator, recorder = main_initialization(config)
+        # for epoch in range(config.epochs):
+        #     training_metrics = algorithm.train_step(epoch=epoch)
+        #     evaluation_metrics = evaluator.eval_step(epoch=epoch)
+        #     # combine training and evaluation metrics
+        #     metrics = deep_update(training_metrics, evaluation_metrics)
+        #     # start a new thread to record the iteration, this speeds up the overall training
+        #     recorder.record_iteration(iteration=epoch, recorded_values=metrics)
+        #
+        # # # close wandb, save the final model, ...
+        # recorder.finalize()
     except Exception:
         traceback.print_exc(file=sys.stderr)
         raise
--- src/calabi_yau_metrics/util/util.py
+++ src/calabi_yau_metrics/util/util.py
@@ -1,6 +1,45 @@
+from omegaconf import OmegaConf
+
+
 def load_omega_conf_resolvers():
     OmegaConf.register_new_resolver("sub_dir_shortener", shortener)
     OmegaConf.register_new_resolver(
         "format", lambda inpt, formatter: formatter.format(inpt)
     )
     OmegaConf.register_new_resolver("conditional_resolver", conditional_resolver)
+
+def shortener(input_string: str | None = None, length=3, show_config_stack=False):
+    if input_string is None:
+        return "job_type"
+    output_parts = []
+
+    for part in input_string.split(","):
+        key, value = part.split("=")
+        modified_key = ""
+
+        key_parts = key.split(".")
+        if not show_config_stack:
+            key_parts = key_parts[-1:]
+        for key_part in key_parts:
+            for word in key_part.split("_"):
+                modified_key += word[:length] + "_"
+            modified_key = modified_key[:-1] + "."
+        modified_key = modified_key[:-1]
+
+        output_parts.append(f"{modified_key}={value}")
+
+    return ",".join(output_parts)
+
+
+def conditional_resolver(condition, if_true: str, if_false: str):
+    if condition:
+        return if_true
+    else:
+        return if_false
+
+
+
+if __name__ == "__main__":
+    inpt = "algorithm.posterior_learner.lnpdf.likelihood.mesh_std=0.02,env.debug.max_tasks_per_split=5"
+    print(shortener(inpt, 3, False))
+

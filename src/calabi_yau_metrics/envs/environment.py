bedf376ac5a89b35c52893606c7a8c930f588fd0
Philipp Dahlinger <philipp.dahlinger@gmx.net>
2024-07-11 17:02:24 +0200

    final eval

--- src/calabi_yau_metrics/envs/environment.py
+++ src/calabi_yau_metrics/envs/environment.py
@@ -6,10 +6,23 @@ import MLGeometry
 class Environment:
     def __init__(self, config):
         self.config = config
+        z0, z1, z2, z3, z4 = sp.symbols('z0, z1, z2, z3, z4')
+        Z = [z0, z1, z2, z3, z4]
         if config.name == "fermat_quintett":
-            z0, z1, z2, z3, z4 = sp.symbols('z0, z1, z2, z3, z4')
-            Z = [z0, z1, z2, z3, z4]
             f = z0 ** 5 + z1 ** 5 + z2 ** 5 + z3 ** 5 + z4 ** 5 + 0.5 * z0 * z1 * z2 * z3 * z4
+        elif config.name == "phi_function":
+            phi = 0.5
+            psi = 0.5
+            f = z0 ** 5 + z1 ** 5 + z2 ** 5 + z3 ** 5 + z4 ** 5 + psi * z0 * z1 * z2 * z3 * z4
+            f = f + phi * (z3 * z4 ** 4 + z3 ** 2 * z4 ** 3 + z3 ** 3 * z4 ** 2 + z3 ** 4 * z4)
+        elif config.name == "alpha_function":
+            alpha = 0.5
+            psi = 0.5
+            f = z0 ** 5 + z1 ** 5 + z2 ** 5 + z3 ** 5 + z4 ** 5 + psi * z0 * z1 * z2 * z3 * z4
+            f = f + alpha * (
+                        z2 * z0 ** 4 + z0 * z4 * z1 ** 3 + z0 * z2 * z3 * z4 ** 2 + z3 ** 2 * z1 ** 3 + z4 * z1 ** 2 * z2 ** 2 + z0 * z1 * z2 * z3 ** 2 +
+                        z2 * z4 * z3 ** 3 + z0 * z1 ** 4 + z0 * z4 ** 2 * z2 ** 2 + z4 ** 3 * z1 ** 2 + z0 * z2 * z3 ** 3 + z3 * z4 * z0 ** 3 + z1 ** 3 * z4 ** 2 +
+                        z0 * z2 * z4 * z1 ** 2 + z1 ** 2 * z3 ** 3 + z1 * z4 ** 4 + z1 * z2 * z0 ** 3 + z2 ** 2 * z4 ** 3 + z4 * z2 ** 4 + z1 * z3 ** 4)
         else:
             raise NotImplementedError()
 
